f2c5b337002c884046590060aefff4d3a1c9fbc6
takuhase <hasetakunoid3228@icloud.com>
2020-03-26 02:45:08 +0900

    Create guest user login function

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'homes#index'
-  get 'homes/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post '/homes/guest_sign_in', to: 'homes#new_guest'
 end
