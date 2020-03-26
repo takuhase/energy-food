1c844ea9cb3d71f449fcac967348ead22c68b857
takuhase <hasetakunoid3228@icloud.com>
2020-03-26 09:40:08 +0000

    Create model spec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  # 姓、名、メール、パスワードがあれば有効な状態であること
+  it "is valid with a first name, last name, email, and password"
+  # 名がなければ無効な状態であること
+  it "is invalid without a first name"
+  # 姓がなければ無効な状態であること
+  it "is invalid without a last name"
+  # メールアドレスがなければ無効な状態であること
+  it "is invalid without an email address"
+  # 重複したメールアドレスなら無効な状態であること
+  it "is invalid with a duplicate email address"
+  # ユーザーのフルネームを文字列として返すこと
+  it "returns a user's full name as a string"
+
+end
