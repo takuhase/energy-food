69e1002eb72ce1cf46dfcbe729e1b8e482999fb5
takuhase <hasetakunoid3228@icloud.com>
2020-03-30 16:38:10 +0900

    Add test scenario for Order model

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -6,6 +6,12 @@ RSpec.describe Order, type: :model do
     expect(FactoryBot.build(:order)).to be_valid
   end
 
+  it "is valid that more than 1 order belong to a user" do
+    first_order = FactoryBot.create(:order)
+    second_order = FactoryBot.build(:order, user_id: first_order.user_id)
+    expect(second_order.save).to be_truthy
+  end
+
   # Exceptional scenarios
   it "is invalid without foreign key user_id" do
     order = FactoryBot.build(:order, user_id: nil)
