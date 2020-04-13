e05fc5e2ba18b4d21003e4dce66c8336b7639aeb
takuhase <hasetakunoid3228@icloud.com>
2020-04-13 13:43:59 +0900

    Create orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,13 @@
 class OrdersController < ApplicationController
+  def create
+    @order = Order.new(user_id: current_user.id)
+    ids = params[:daily_food_ids]
+    if @order.save
+      ids.each do |id|
+        OrderDetail.create(order_id: @order.id, daily_food_id: id)
+      end
+    else
+      # redirect_to root with alert
+    end
+  end
 end
