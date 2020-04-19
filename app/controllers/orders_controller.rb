60827dcbccc039e770b086a95c51ebf7ed6bc73a
takuhase <hasetakunoid3228@icloud.com>
2020-04-19 14:30:02 +0900

    Redirect to root if order is submitted without chosing any foods.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,17 +4,13 @@ class OrdersController < ApplicationController
   def create
     @order = Order.new(user_id: current_user.id)
     ids = params[:daily_food_ids]
-    # redirect_to(root_path, alert: 'メニューを選択して下さい') if ids == nil
 
-    if @order.save
-      ids.each do |id|
-        # OrderDetail.create(order_id: @order.id, daily_food_id: id)
-        OrderDetail.create!(order_id: @order.id, daily_food_id: id.to_i)
-      end
-      redirect_to root_path, notice: '注文を受け付けました。'
-    else
-      # redirect_to root with alert "Pls login or Singin first"
+    return redirect_to root_path, alert: 'メニューを選択して下さい。' if ids.nil?
+    @order.save
+    ids.each do |id|
+      OrderDetail.create!(order_id: @order.id, daily_food_id: id.to_i)
     end
+    redirect_to root_path, notice: '注文を受け付けました。'
 
   end
 end
