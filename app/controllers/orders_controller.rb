c19d6ddc627b9460cb04d89fc16f81fbf5c73d84
takuhase <hasetakunoid3228@icloud.com>
2020-04-22 17:29:49 +0900

    Create edit view and destroy function

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,6 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
+  before_action :correct_user, only: [:edit]
 
   def create
     @order = Order.new(user_id: current_user.id)
@@ -10,7 +11,36 @@ class OrdersController < ApplicationController
     ids.each do |id|
       OrderDetail.create!(order_id: @order.id, daily_food_id: id.to_i)
     end
-    redirect_to root_path, notice: '注文を受け付けました。'
+    redirect_to request.referrer, notice: '注文を受け付けました。'
+  end
 
+  def edit
+    @order = Order.find(params[:id])
+    order_details = OrderDetail.where(order_id: @order.id)
+    @daily_foods = return_daily_foods_from_order_details(order_details)
+    # @tomorrow_foods = show_menu_list(Date.tomorrow)
+
+  end
+
+  def destroy
+    order = Order.find(params[:id])
+    order.destroy
+    redirect_to request.referrer, notice: '注文をキャンセルしました。'
+  end
+
+  private
+  def correct_user
+    @user = Order.find(params[:id]).user
+    redirect_to(root_url, alert: '他ユーザーの情報は閲覧できません') unless current_user == @user
+  end
+
+  # def return_order_details_from_order(order)
+  #   OrderDetail.where(order_id: order.id)
+  # end
+
+  def return_daily_foods_from_order_details(order_details)
+    order_details.map do |order_detail|
+      DailyFood.find(order_detail.daily_food_id)
+    end
   end
 end
