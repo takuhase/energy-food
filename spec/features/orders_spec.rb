b05b8362f70b89af8ccffa254f7e2b2b6ac55c7a
takuhase <hasetakunoid3228@icloud.com>
2020-04-27 13:02:12 +0900

    Complete feature spec for order function

--- spec/features/orders_spec.rb
+++ spec/features/orders_spec.rb
@@ -1,24 +1,22 @@
 require 'rails_helper'
 
 RSpec.feature "Orders", type: :feature do
-  let!(:test_user) { FactoryBot.create(:user) }
   let!(:daily_food_1) { FactoryBot.create(:food_date_relation_1) }
   let!(:daily_food_2) { FactoryBot.create(:food_date_relation_2) }
   let!(:daily_food_3) { FactoryBot.create(:food_date_relation_3) }
   let!(:daily_food_4) { FactoryBot.create(:food_date_relation_4) }
+  let!(:test_user) { FactoryBot.create(:user) }
 
-  scenario "Create Order correctly" do
+  scenario "Create, update and delete order successfully" do
     login_as test_user, scope: :user
     visit user_path(test_user)
     expect(current_path).to eq "/users/#{test_user.id}"
 
+    # Create order
     expect do
       expect(page).to have_content "明日のメニュー"
-      expect(page).to have_content "注文履歴"
-
       within(".select-menu-#{daily_food_1.id}") do
         expect(page).to have_content "蜜汁鸡丁"
-        check "daily_food_ids[]"
       end
       within(".select-menu-#{daily_food_2.id}") do
         expect(page).to have_content "姜蓉蒸鸡"
@@ -27,7 +25,43 @@ RSpec.feature "Orders", type: :feature do
         expect(page).to have_content "蒸西兰花"
         check "daily_food_ids[]"
       end
+      within(".order-history") do
+        expect(page).to have_content "注文履歴"
+        expect(page).not_to have_content "蜜汁鸡丁"
+        expect(page).not_to have_content "蒸西兰花"
+        expect(page).not_to have_content "姜蓉蒸鸡"
+      end
       click_button "注文を送信"
     end.to change(Order, :count).by(1)
+
+    # Update order
+    within(".order-history") do
+      expect(page).not_to have_content "蜜汁鸡丁"
+      expect(page).not_to have_content "姜蓉蒸鸡"
+      expect(page).to have_content "蒸西兰花"
+      click_link "Edit"
+    end
+    within(".select-menu-#{daily_food_1.id}") do
+      expect(page).to have_content "蜜汁鸡丁"
+      check "daily_food_ids[]"
+    end
+    within(".select-menu-#{daily_food_2.id}") do
+      expect(page).to have_content "姜蓉蒸鸡"
+    end
+    within(".select-menu-#{daily_food_4.id}") do
+      expect(page).to have_content "蒸西兰花"
+      uncheck "daily_food_ids[]"
+    end
+    click_button "注文を変更"
+
+    # Delete order
+    expect do
+      within(".order-history") do
+        expect(page).to have_content "蜜汁鸡丁"
+        expect(page).not_to have_content "姜蓉蒸鸡"
+        expect(page).not_to have_content "蒸西兰花"
+        click_link "Delete"
+      end
+    end.to change(Order, :count).by(-1)
   end
 end
