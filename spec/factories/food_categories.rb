3cb02bba4f814718b6db7e9a84c9b3a27a9c24f8
takuhase <hasetakunoid3228@icloud.com>
2020-04-28 22:36:22 +0900

    Add some code to display grouped foods.

--- spec/factories/food_categories.rb
+++ spec/factories/food_categories.rb
@@ -1,16 +1,39 @@
 FactoryBot.define do
   factory :chicken, class: "FoodCategory" do
+    id { 1 }
     name { "Chicken" }
     association :nutrient, factory: :meat
+
+    initialize_with do
+      FoodCategory.find_or_initialize_by(
+        id: 1,
+        name: "Chicken"
+      )
+    end
+
   end
 
   factory :pork, class: "FoodCategory" do
     name { "Pork" }
     association :nutrient, factory: :meat
+
+    initialize_with do
+      FoodCategory.find_or_initialize_by(
+        id: 2,
+        name: "Pork"
+      )
+    end
   end
 
   factory :vege, class: "FoodCategory" do
     name { "Vege" }
     association :nutrient, factory: :vegetable
+
+    initialize_with do
+      FoodCategory.find_or_initialize_by(
+        id: 5,
+        name: "Vege"
+      )
+    end
   end
 end
