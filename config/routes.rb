dcbbe93f937fd39e19f33bb7340d20c2628df34e
takuhase <hasetakunoid3228@icloud.com>
2020-05-19 03:50:06 +0900

    Add ShopClerk model

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
+  # devise_for :shop_clerks
+
+  devise_for :shop_clerks, controllers: {
+  sessions: 'shop_clerks/sessions',
+  passwords: 'shop_clerks/passwords',
+  registrations: 'shop_clerks/registrations'
+  }
+
+
+
+
   resources :likes, only: [:create, :destroy]
   devise_for :users
-  # resources :orders, only: [:create, :update]
+
   resources :orders
   resources :users, only: [:show]
   root 'homes#index'
