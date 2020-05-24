ef9f04ed91468471558c6717f2914f34b749278d
takuhase <hasetakunoid3228@icloud.com>
2020-05-25 02:35:07 +0900

    Add test scenarios for Admin login/logout function

--- spec/features/sign_ins_spec.rb
+++ spec/features/sign_ins_spec.rb
@@ -3,16 +3,17 @@ require 'rails_helper'
 RSpec.feature "SignIns", type: :feature do
   let(:user) { create(:user) }
   let(:shop_clerk) { create(:shop_clerk) }
+  let(:admin_user) { create(:admin_user) }
 
-  scenario "Use can sign in successfully" do
+  scenario "User can sign in successfully" do
     visit new_user_session_path
     click_link "ログイン"
     expect(page).to have_content "ログイン"
     fill_in "session-email", with: user.email
     fill_in "session-password", with: user.password
     click_button "ログイン"
-    expect(page).to have_content "ログインしました。"
     expect(page).to have_current_path "/"
+    expect(page).to have_content "ログインしました。"
     expect(page).to have_content "明日のメニュー"
   end
 
@@ -20,9 +21,20 @@ RSpec.feature "SignIns", type: :feature do
     visit new_shop_clerk_session_path
     fill_in "shop_clerk[email]", with: shop_clerk.email
     fill_in "shop_clerk[password]", with: shop_clerk.password
-    click_button "Log in"
-    expect(page).to have_content "ログインしました。"
+    click_button "ログイン"
     expect(page).to have_current_path "/shop_clerks"
+    expect(page).to have_content "ログインしました。"
     expect(page).to have_content "本日の注文一覧"
+  end
+
+  scenario "Admin can sign in successfully" do
+    visit new_admin_user_session_path
+    expect(current_path).to eq "/admin/login"
+    fill_in "admin_user[email]", with: admin_user.email
+    fill_in "admin_user[password]", with: admin_user.password
+    click_button "ログイン"
+    expect(page).to have_current_path "/admin"
+    expect(page).to have_content "ログインしました。"
+    expect(page).to have_content "ダッシュボード"
   end
 end
--- spec/features/sign_outs_spec.rb
+++ spec/features/sign_outs_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.feature "SignOuts", type: :feature do
   let(:user) { create(:user) }
   let(:shop_clerk) { create(:shop_clerk) }
+  let(:admin_user) { create(:admin_user) }
 
   scenario "User can sign out from his own page" do
     login_as user, scope: :user
@@ -20,5 +21,14 @@ RSpec.feature "SignOuts", type: :feature do
     expect(page).to have_content "ログアウトしました。"
     expect(current_path).to eq "/shop_clerks/sign_in"
     expect(page).to have_content "店舗スタッフログイン"
+  end
+
+  scenario "Admin can sign out from his own page" do
+    login_as admin_user, scope: :admin_user
+    visit admin_root_path
+    click_link "ログアウト"
+    expect(page).to have_content "ログアウトしました。"
+    expect(current_path).to eq "/users/sign_in"
+    expect(page).to have_content "ログイン"
   end
 end
