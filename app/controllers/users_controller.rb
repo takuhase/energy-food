59598a7c622f0cb6df37f9d2d9796b43b016642f
takuhase <hasetakunoid3228@icloud.com>
2020-05-28 02:59:49 +0900

    Improve layout for order history

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   before_action :correct_user, only: [:show]
 
   def show
-    @orders = orders = Order.get_orders(params[:id], params[:page])
-    @ordered_foods_history = orders.map(&:daily_foods)
+    @orders = Order.get_orders(params[:id], params[:page])
+    @ordered_foods_history = @orders.map(&:daily_foods)
   end
 end
